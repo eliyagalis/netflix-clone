017be9c1a8d31e35e451ad0b0d44eb9e8af9b973
eliyagalis <eliyagalisbiz@gmail.com>
2025-03-21 01:39:26 +0200

    Add LandingPage and Header components with DaisyUI integration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
+import LandingPage from "./pages/LandingPage"
 
 function App() {
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-200">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind with Vite!</h1>
-    </div>
+    <>
+      <LandingPage />
+    </>
   )
 }
 
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,8 +1,29 @@
+import Header from "../components/shared/Header"
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
+    return (
+        <>
+            <div>
+                <Header />
+                <div className="w-full h-150 overflow-hidden">
+                    <div className="z-1 text-center absolute w-full my-70 flex flex-col justify-center items-center">
+                        <h1 className="text-2xl sm:text-3xl md:text-3xl w-4/10 lg:text-4xl xl:text-5xl text-white font-bold">Unlimited movies, TV shows, and more</h1>
+                        <p className="text-sm sm:text-base md:text-lg w-4/10 lg:text-xl xl:text-xl mt-5 text-white font-medium">Starts at ₪32.90. Cancel anytime.</p>
+                        <p className="text-white mt-7">Ready to watch? Enter your email to create or restart your membership.</p>
+                        <div>
+                            <input className="input validator bg-0" type="email" required placeholder="mail@site.com" />
+                            <div className="validator-hint">Enter valid email address</div>
+                        </div>
+                    </div>
+                    <img
+                        className="brightness-30 z-0 w-full h-full object-cover"
+                        src="https://assets.nflxext.com/ffe/siteui/vlv3/f6e7f6df-6973-46ef-b98f-12560d2b3c69/web/IL-en-20250317-TRIFECTA-perspective_253c970d-8a6c-4257-b0b0-66a78d743927_large.jpg"
+                    />
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default LandingPage
