8c7f2b634596f12857ba4dac889e50ff1d88d4cc
eliyagalis <eliyagalisbiz@gmail.com>
2025-03-20 14:34:19 +0200

    Add Tailwind CSS integration and update App component styling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,9 @@
 function App() {
 
   return (
-    <>
-     Hello Adel and Eliya!!!
-     Why does this not work?
-     work work work
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-200">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind with Vite!</h1>
+    </div>
   )
 }
 
