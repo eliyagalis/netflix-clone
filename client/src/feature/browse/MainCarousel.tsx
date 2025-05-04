378a77d78ec8067d2effe3710a2ac4a0bdf2cb59
eliyagalis <eliyagalisbiz@gmail.com>
2025-05-04 16:06:06 +0300

    refactor: remove unused logout text and improve logout handling in components

--- client/src/feature/browse/MainCarousel.tsx
+++ client/src/feature/browse/MainCarousel.tsx
@@ -7,18 +7,18 @@ import { CarouselItem } from '../../components/shared/carousel/CarouselItem';
 import { MoviePreview } from '../../components/shared/carousel/MoviePreview';
 import { useCarouselHover } from '../../hooks/useCarouselHover';
 import { ISeriesCard } from '../../types/ISeriesCard';
-import { IMovieList, ISeriesList } from '../../types/IMovieList';
+import { ICarouselCard } from '../../types/ICarouselCard';
 
 type CarouselProps = {
-    movies: IMovieList;
+    movies: ICarouselCard[];
     isIndexed?: boolean;
     title?: string;
-    isCarousel:boolean;
+    isCarousel: boolean;
 };
 
 const SCALE = 1.25;
 
-export default function MainCarousel({ movies, isIndexed, title="" ,isCarousel=true}: CarouselProps) {
+export default function MainCarousel({ movies, isIndexed, title = "", isCarousel = true }: CarouselProps) {
     const carouselRef = useRef<HTMLDivElement | any>(null);
     const {
         preview,
@@ -38,23 +38,23 @@ export default function MainCarousel({ movies, isIndexed, title="" ,isCarousel=t
 
     return (
         <div className='my-5'>
-            <Typography size={`${typography.small}`} className='font-semibold'>{title? title:""}</Typography>
+            <Typography size={`${typography.small}`} className='font-semibold'>{title ? title : ""}</Typography>
             <div className="relative flex items-center px-0">
                 {/* Left arrow */}
-                {isCarousel&& (
-                  <CarouselArrow direction="left" onClick={() => scrollCarousel('left')} />
+                {isCarousel && (
+                    <CarouselArrow direction="left" onClick={() => scrollCarousel('left')} />
                 )}
 
                 {/* track */}
                 <div
                     ref={carouselRef}
-                    className={` ${isCarousel? "carousel overflow-visible overflow-x-scroll flex snap-x snap-mandatory p-2 py-5"
-                        : "grid-cols-5 grid-rows-7 p-2 my-7" }`}
+                    className={` ${isCarousel ? "carousel overflow-visible overflow-x-scroll flex snap-x snap-mandatory p-2 py-5"
+                        : "grid-cols-5 grid-rows-7 p-2 my-7"}`}
                 >
-                    {movies.map((movie:IMovieCard|ISeriesCard, idx:number) => (
+                    {movies.map((movie: ICarouselCard, idx: number) => (
                         <CarouselItem
                             key={movie.title}
-                            item={movie as ISeriesCard|IMovieCard}
+                            item={movie as ISeriesCard | IMovieCard}
                             index={idx}
                             isIndexed={isIndexed}
                             onMouseEnter={handleEnterTile}
@@ -64,7 +64,7 @@ export default function MainCarousel({ movies, isIndexed, title="" ,isCarousel=t
                 </div>
 
                 {/* Right arrow */}
-                {isCarousel&& (
+                {isCarousel && (
                     <CarouselArrow direction="right" onClick={() => scrollCarousel('right')} />
                 )}
                 {/* Preview */}
--- client/src/pages/layouts/SignupLayout.tsx
+++ client/src/pages/layouts/SignupLayout.tsx
@@ -1,25 +1,51 @@
 import Header from '../../components/shared/Header'
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useNavigate } from 'react-router-dom'
 import { typography } from '../../data/typography'
 import { colors } from '../../data/colors'
 import Footer from '../../components/shared/Footer'
-import { useAppSelector } from '../../store/store'
+import { useAppDispatch, useAppSelector } from '../../store/store'
+import { resetProfiles } from '../../store/slices/profilesSlice'
+import { logout } from '../../store/slices/authSlice'
+import { logoutRequest } from '../../api/authApi'
 
 type SignUpPageProps = {
     children?: React.ReactNode
 }
 
 const SignupLayout: React.FC<SignUpPageProps> = () => {
-    const auth = useAppSelector((state)=>state.auth)
+    const auth = useAppSelector((state) => state.auth)
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const logoutHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        dispatch(resetProfiles());
+        dispatch(logout());
+        logoutRequest();
+        navigate('/logout');
+    }
 
     return (
         <div className={`text-${colors.text.primary} bg-white min-h-screen w-full flex flex-col`}>
             <Header link='/' border>
-                
-                    <Link to={auth.user ? '/logout': '/login'}
-                        className={`${typography.small} hover:underline font-medium`}>
-                        {auth.user ? 'Log Out': 'Sign In'}
-                    </Link>
+
+                {
+                    auth.user ? (
+                        <button
+                            className={`${typography.small} hover:underline font-medium`}
+                            onClick={() => logoutHandler}
+                        >
+                            Log Out
+                        </button>
+                    ) : (
+                        <Link to={'/login'}
+                            className={`${typography.small} hover:underline font-medium`}>
+                            {'Sign In'}
+                        </Link>
+                    )
+                }
+
+
             </Header>
 
             <main className="flex-1 max-w-270 w-11/12 mx-auto flex flex-col">
@@ -31,7 +57,7 @@ const SignupLayout: React.FC<SignUpPageProps> = () => {
                 border-t-[0.5px] border-[rgb(230,230,230)] 
                 font-semibold ${colors.text.lightGray}
             `} />
-        </div>
+        </div >
     )
 }
 
